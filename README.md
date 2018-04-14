36a1fe1b0d298ec2bf08cd81ff7a196adcb163b7
OsosKP <ososkp@gmail.com>
2018-04-14 08:58:27 +0100

    Edited readme

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ MLB.tv
 weather.sh
 	Modification of r3donnx's script to run weather information as widget
 	on Mac touchbar using Better Touch Tool. This version reads plaintext
-	rather than XML, because the source XML is now plaintext (for some reason?"
+	rather than XML, because the source XML is now plaintext (for some reason?)
+
 
--- weather.sh
+++ weather.sh
@@ -4,14 +4,10 @@
 
 #!/bin/sh
 w_xml=$(curl --silent "https://weather.tuxnet24.de/?id=560743&amp;mode=xml");
-# w_txt=$(xmllint --xpath "string(//current_text)" - <<<"$w_xml" | xargs);
-# awk '$1 == "current_text" {w_txt=$3}' - <<< "$w_xml" | xargs
-# "$w_xml" > awk '/current_text/ {echo $3}'
 line=$(echo "$w_xml" | grep current_text)
 words=( $line )
 w_txt=${words[2]}
-# w_tpc=$(xmllint --xpath "string(//current_temp)"  - <<<"$w_xml" | xargs); w_tpc=${w_tpc//[[:blank:]]/};
-# awk '/current_temp/ {w_tpc=$3}' - <<< "$w_xml" | xargs
+
 line=$(echo "$w_xml" | grep current_temp)
 words=( $line )
 w_tpc=${words[2]}
