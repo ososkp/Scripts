b29f84384714e4c1a7c38dd5f32298d5720b70bb
OsosKP <ososkp@gmail.com>
2018-04-20 13:08:29 +0100

    Fixed issue where Mostly Sunny/Cloudy would show the word mostly instead of the appropriate icon

--- weather.sh
+++ weather.sh
@@ -8,19 +8,22 @@ line=$(echo "$w_xml" | grep current_text)
 words=( $line )
 w_txt=${words[2]}
 
+if [ "$w_txt" == "Mostly" ]; then w_helper=${words[3]};
+fi
+
 line=$(echo "$w_xml" | grep current_temp)
 words=( $line )
 w_tpc=${words[2]}
 
 if   [ "$w_txt" == "Sunny" ]; then w_sym="☼";
-elif [ "$w_txt" == "Mostly Sunny" ]; then w_sym="☼";
+elif [ "$w_txt" == "Mostly" ] && [ "$w_helper" == "Sunny" ]; then w_sym="☼";
 elif [ "$w_txt" == "Showers" ]; then w_sym="☂";
 elif [ "$w_txt" == "Clear" ]; then w_sym="☾";
 elif [ "$w_txt" == "Thunderstorms" ]; then w_sym="⚡";
 elif [ "$w_txt" == "Scattered Thunderstorms" ]; then w_sym="☔";
 elif [ "$w_txt" == "Isolated Thundershovers" ]; then w_sym="☔";
 elif [ "$w_txt" == "Cloudy" ]; then w_sym="☁";
-elif [ "$w_txt" == "Mostly Cloudy" ]; then w_sym="☁";
+elif [ "$w_txt" == "Mostly" ] && [ "$w_helper" == "Cloudy" ]; then w_sym="☁";
 elif [ "$w_txt" == "Partly Cloudy" ]; then w_sym="☁";
 elif [ "$w_txt" == "Partly" ]; then w_sym="☁";
 elif [ "$w_txt" == "Breezy" ]; then w_sym="⚐";
